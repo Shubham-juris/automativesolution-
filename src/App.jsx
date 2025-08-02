5e506ed8d2803314f369a02d521160f0c525c4e3
Vinay730 <vinay.juris@gmail.com>
2025-08-02 10:29:59 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/common/Navbar";
 import PageRoutes from "./routes/PageRoutes";
 import Footer from "./components/common/Footer";
-import FloatingTruck from "./components/floting truk/FloatingTruck";
 
 function App() {
   return (
     <>
       <Navbar />
-      <FloatingTruck />
       <PageRoutes />
       <Footer />
     </>
